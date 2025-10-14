1e89c95737db8fb86f20009fcd11c2a98fab3609
Dolecto <junkevinshin@gmail.com>
2025-10-14 21:54:24 +0200

    feat: add ball and walls

--- godot/player.gd
+++ godot/player.gd
@@ -6,10 +6,14 @@ extends CharacterBody3D
 
 var target_velocity = Vector3.ZERO
 
+# Currently, applied force is dependent on the speed of the player
+# TODO: - add a "push" to apply on the ball
+#       - fix jank when it comes to interacting with the ball
+
 # _physics runs on every frame, dependent on the application
 # _physics_process runs at a specified frame rate (default 60/s)
 func _physics_process(delta):
-	var direction = Vector3.ZERO # store input
+	var direction = Vector3.ZERO # store input direction
 	
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1 # absolute direction
@@ -26,7 +30,7 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
-		
+	
 	# Ground velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
@@ -36,7 +40,7 @@ func _physics_process(delta):
 		# fall_acceleration is in m/s^2, delta is in s
 		# final_v = initial_v + (-g * T)
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-		
+	
 	velocity = target_velocity
 	move_and_slide()
 	
